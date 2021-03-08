b72955d39cc785b50e69a19786ec68a3ca992623
selvarajpanneer <selvapanneer2020@gmail.com>
2021-03-08 09:38:30 +0530

    simplenoteapp-version-1 -updated with notification.

--- app/src/main/java/com/example/simplenoteapp/MyBroadCastReceiver.java
+++ app/src/main/java/com/example/simplenoteapp/MyBroadCastReceiver.java
@@ -4,23 +4,25 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+
 import androidx.core.app.NotificationCompat;
 
 public class MyBroadCastReceiver extends BroadcastReceiver {
 
     private SqlHelper dpHelper;
+
     @Override
     public void onReceive(Context context, Intent intent) {
-        Intent intent1=intent;
-        int serial_no=intent1.getIntExtra("notification_slno",0);
-        String title=intent1.getStringExtra("notification_title");
-        String content=intent1.getStringExtra("notification_content");
+        Intent intent1 = intent;
+        int serial_no = intent1.getIntExtra("notification_slno", 0);
+        String title = intent1.getStringExtra("notification_title");
+        String content = intent1.getStringExtra("notification_content");
         NotificationService notificationHelper = new NotificationService(context);
-        NotificationCompat.Builder nb = notificationHelper.getChannelNotification(title,content);
-        Intent intent2 = new Intent(context,InputSection.class);
-        intent2.putExtra("edit_slno",serial_no);
-        intent2.putExtra("edit_title",title);
-        intent2.putExtra("edit_content",content);
+        NotificationCompat.Builder nb = notificationHelper.getChannelNotification(title, content);
+        Intent intent2 = new Intent(context, InputSection.class);
+        intent2.putExtra("edit_slno", serial_no);
+        intent2.putExtra("edit_title", title);
+        intent2.putExtra("edit_content", content);
         PendingIntent contentIntent = PendingIntent.getActivity(context, 0,
                 intent2, PendingIntent.FLAG_UPDATE_CURRENT);
         nb.setContentIntent(contentIntent);
@@ -30,9 +32,10 @@ public class MyBroadCastReceiver extends BroadcastReceiver {
         getdbHelper(context).deactivateAlarm(serial_no);
 
     }
-    public SqlHelper getdbHelper(Context context){
-        if(dpHelper==null){
-            dpHelper= new SqlHelper(context);
+
+    public SqlHelper getdbHelper(Context context) {
+        if (dpHelper == null) {
+            dpHelper = new SqlHelper(context);
         }
         return dpHelper;
     }
